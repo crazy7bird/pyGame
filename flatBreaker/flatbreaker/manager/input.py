4926ae9a217157c5d671fc301d226b0a2ee958e5
crazybird <roy7alexandre@gmail.com>
2024-02-05 18:53:15 +0100

    Generic release system for keyboard.

--- flatBreaker/flatbreaker/manager/input.py
+++ flatBreaker/flatbreaker/manager/input.py
@@ -18,30 +18,40 @@ class keyboardControler :
         self.bMoveWall = False
         self.reloadBall = False
         self.player = player
+        self.pressedKey = [key]
+
+    def singlePushClean(self) -> None :
+        # retains key if they still pushed.
+        self.pressedKey = [key for key in self.pressedKey if self.keys[key]]
+
+    def isReleased(self,key) -> bool :
+        if key in self.pressedKey :
+            return False
+        #We add it cause we dont check it for nothing
+        self.pressedKey.append(key)
+        return True
 
     def update(self,dt) -> None :
-        if self.keys[key.SPACE]:
+        self.singlePushClean()
+        if self.keys[key.SPACE] and self.isReleased(key.SPACE) :
             if(self.colider.ballOnBoat()):
                 self.colider.unlockBalls()
             else :
                 if(self.player.getMun()> 0) :
                     self.player.addMuns(-1)
                     self.colider.shoot()
+
         if self.keys[key.A]:
             self.boat.moveLeft(dt)
         if self.keys[key.I] :
             self.boat.moveRight(dt)
 
-        if self.keys[key.B] and not self.bMoveWall:
+        if self.keys[key.B] and self.isReleased(key.B) :
             self.wall.test_move_row()
-            self.bMoveWall = True
-        elif  not self.keys[key.B]:
-            self.bMoveWall = False
+            #self.addPressedKey(key.B)
 
-        if self.keys[key.O] and not self.reloadBall :
+        if self.keys[key.O] and self.isReleased(key.O) :
             if(self.player.getBalls()>0 and self.colider.ballOnBoat() == False) :
                 self.colider.creatBall()
                 self.player.addBalls(-1)
-                self.reloadBall = True
-        elif not self.keys[key.O] :
-            self.reloadBall = False
+                #self.addPressedKey(key.O)
