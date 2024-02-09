5a2ed1970b064c2c4d53f85757ab0999b13a9114
crazybird <roy7alexandre@gmail.com>
2024-02-09 20:02:40 +0100

    [WIP] Character fondation

--- flatBreaker/flatbreaker/farmer_test.py
+++ flatBreaker/flatbreaker/farmer_test.py
@@ -3,17 +3,22 @@ from objects.characters.nativs.farmer import farmer
 
 
 
+
 if __name__ == "__main__" :
     window = pyglet.window.Window()
 
     window.set_fullscreen(True)
 
     farmer = farmer( x= window.width/2)
+    farmer.moveForward()
+    farmer.moveBackward()
     @window.event
     def on_draw():
         window.clear()
+        farmer.move(-5,0)
         farmer.draw()
 
     # hiding mouse
     window.set_mouse_visible(False)
     pyglet.app.run()
+
--- flatBreaker/flatbreaker/objects/characters/nativs/farmer.py
+++ flatBreaker/flatbreaker/objects/characters/nativs/farmer.py
@@ -19,12 +19,21 @@ class farmer():
             self.forwardImg.append(pyglet.image.load(Path(SPRIT_PATH, f"Farmer{i+N_IMG_STILL}.png")))
         for i in range(N_IMG_MOVING) :
             self.backwardImg.append(pyglet.image.load(Path(SPRIT_PATH, f"Farmer{i+N_IMG_STILL}.png")))
-            self.backwardImg[i].width -= X_SIZE
+            self.backwardImg[i] = self.backwardImg[i].get_texture().get_transform(True)
+
         self.stillAnim = pyglet.image.Animation.from_image_sequence(self.stillImg, duration=0.5, loop=True)
-        self.forwardAnim = pyglet.image.Animation.from_image_sequence(self.forwardImg, duration=0.5, loop=True)
-        self.backwardAnim = pyglet.image.Animation.from_image_sequence(self.backwardImg, duration=0.5, loop = True)
+        self.forwardAnim = pyglet.image.Animation.from_image_sequence(self.forwardImg, duration=0.1, loop=True)
+        self.backwardAnim = pyglet.image.Animation.from_image_sequence(self.backwardImg, duration=0.1, loop = True)
         self.spr = pyglet.sprite.Sprite(self.stillAnim, x=x, y=0)
     
+    def standStill(self) -> None :
+        self.spr = pyglet.sprite.Sprite(self.stillAnim, x=self.spr.x, y=0)
+
+    def moveForward(self) -> None :
+        self.spr = pyglet.sprite.Sprite(self.forwardAnim, x=self.spr.x, y=0)
+    def moveBackward(self) -> None :
+        self.spr = pyglet.sprite.Sprite(self.backwardAnim, x=self.spr.x, y=0)
+
     def move(self,x,y) -> None :
         self.spr.x += x
         self.spr.y += y
